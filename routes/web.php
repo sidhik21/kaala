2317412c4e6ea0f973fecc1e5631e526b2e361fd
Abubakkar Sidhik <sidhikoff21@gmail.com>
2026-06-11 15:35:12 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,17 @@
-
 <?php
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\KaalaController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
 });
 
 Route::get('/kaala',   [KaalaController::class, 'showLogin'])->name('login');
 Route::post('/kaala',  [KaalaController::class, 'login']);  // ← changed to /kaala
 
 Route::get('/feed',    [KaalaController::class, 'showFeed'])->name('feed');
-Route::post('/feed',   [KaalaController::class, 'submitFeed']);
+Route::post('/feed',   [KaalaController::class, 'submitFeed'])->name('feed.form');
 Route::get('/thankyou',[KaalaController::class, 'thankYou'])->name('thankyou');
 Route::post('/logout', [KaalaController::class, 'logout'])->name('logout');
 Route::get('/list',    [KaalaController::class, 'show'])->name('list');
