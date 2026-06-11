079c9e2ea363e7fea06659eb15b68457577d0415
sidhik21 <sidhikoff21@gmail.com>
2026-06-11 16:38:50 +0530

    fix: force HTTPS on production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -20,5 +21,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrap();
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
